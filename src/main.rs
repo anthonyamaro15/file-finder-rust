dbd3b1a03965cc87cf7637d44cf33d260d814dd4
Anthony Amaro <anthonyamaro5555@gmail.com>
2024-06-10 18:26:26 -0700

    feat: setup basic working tui

--- src/main.rs
+++ src/main.rs
@@ -1,259 +1,11 @@
-/* mod tui;
-
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use std::{error::Error, fs, io};
-
-use ratatui::{
-    prelude::*,
-    widgets::{Block, List, ListItem, ListState, Paragraph, StatefulWidget},
+use crossterm::event::EnableMouseCapture;
+use std::{
+    fs,
+    io::{self, Stdout},
+    process::Command,
 };
 
-enum InputMode {
-    Normal,
-    Editing,
-}
-
-struct App {
-    input: String,
-    character_index: usize,
-    input_mode: InputMode,
-    message: Vec<String>,
-    files: Vec<String>,
-}
-
-impl App {
-    const fn new(files: Vec<String>) -> Self {
-        Self {
-            input: String::new(),
-            input_mode: InputMode::Normal,
-            message: Vec::new(),
-            files,
-            character_index: 0,
-        }
-    }
-
-    fn move_cursor_left(&mut self) {
-        let cursor_moved_left = self.character_index.saturating_sub(1);
-        self.character_index = self.clamp_cursor(cursor_moved_left);
-    }
-
-    fn move_cursor_right(&mut self) {
-        let cursor_moved_right = self.character_index.saturating_add(1);
-        self.character_index = self.clamp_cursor(cursor_moved_right);
-    }
-
-    fn enter_char(&mut self, new_char: char) {
-        let index = self.byte_index();
-        self.input.insert(index, new_char);
-        self.filter_files(self.input.clone());
-        self.move_cursor_right();
-    }
-
-    fn filter_files(&mut self, input: String) {
-        let filtered_files: Vec<String> = self
-            .files
-            .iter()
-            .filter(|file| file.contains(&input))
-            .map(|file| file.clone())
-            .collect();
-        self.files = filtered_files;
-    }
-
-    fn byte_index(&mut self) -> usize {
-        self.input
-            .char_indices()
-            .map(|(i, _)| i)
-            .nth(self.character_index)
-            .unwrap_or(self.input.len())
-    }
-
-    fn delete_char(&mut self) {
-        let is_not_cursor_leftmost = self.character_index != 0;
-        if is_not_cursor_leftmost {
-            let current_index = self.character_index;
-            let from_left_to_current_index = current_index - 1;
-
-            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
-
-            let after_char_to_delete = self.input.chars().skip(current_index);
-
-            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
-            self.filter_files(self.input.clone());
-            self.move_cursor_left();
-        }
-    }
-
-    fn clamp_cursor(&mut self, new_cursor_pos: usize) -> usize {
-        new_cursor_pos.clamp(0, self.input.chars().count())
-    }
-
-    fn reset_cursor(&mut self) {
-        self.character_index = 0;
-    }
-
-    fn submit_message(&mut self) {
-        self.message.push(self.input.clone());
-        self.input.clear();
-        self.reset_cursor();
-    }
-}
-
-fn main() -> io::Result<()> {
-    let entries = fs::read_dir("../../")?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()?;
-
-    let mut file_strings: Vec<String> = Vec::new();
-
-    for value in entries.iter() {
-        let val = value.clone().into_os_string().to_str().unwrap().to_string();
-
-        file_strings.push(val.clone());
-    }
-
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
-    let app = App::new(file_strings.clone());
-    let res = run_app(&mut terminal, app);
-
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-
-    terminal.show_cursor()?;
-
-    if let Err(err) = res {
-        println!("{err:?}");
-    }
-
-    println!("whar are values, {:?}", file_strings);
-
-    Ok(())
-}
-
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
-    loop {
-        terminal.draw(|f| ui(f, &app))?;
-
-        if let Event::Key(key) = event::read()? {
-            match app.input_mode {
-                InputMode::Normal => match key.code {
-                    KeyCode::Char('e') => {
-                        app.input_mode = InputMode::Editing;
-                    }
-                    KeyCode::Char('q') => {
-                        return Ok(());
-                    }
-                    _ => {}
-                },
-
-                InputMode::Editing if key.kind == KeyEventKind::Press => match key.code {
-                    KeyCode::Enter => app.submit_message(),
-                    KeyCode::Char(to_insert) => {
-                        app.enter_char(to_insert);
-                    }
-                    KeyCode::Backspace => {
-                        app.delete_char();
-                    }
-                    KeyCode::Left => {
-                        app.move_cursor_left();
-                    }
-                    KeyCode::Right => {
-                        app.move_cursor_right();
-                    }
-                    KeyCode::Esc => {
-                        app.input_mode = InputMode::Normal;
-                    }
-                    _ => {}
-                },
-                InputMode::Editing => {}
-            }
-        }
-    }
-}
-
-fn ui(f: &mut Frame, app: &App) {
-    let vertical = Layout::vertical([
-        Constraint::Length(1),
-        Constraint::Length(3),
-        Constraint::Min(1),
-    ]);
-
-    let [help_area, input_area, message_area] = vertical.areas(f.size());
-
-    let (msg, style) = match app.input_mode {
-        InputMode::Normal => (
-            vec![
-                "Press ".into(),
-                "q".bold(),
-                " to exit".into(),
-                "e".bold(),
-                " to start editing".bold(),
-            ],
-            Style::default().add_modifier(Modifier::RAPID_BLINK),
-        ),
-        InputMode::Editing => (
-            vec![
-                "Press ".into(),
-                "Esc".bold(),
-                " to stop editing".into(),
-                "Enter".bold(),
-                " to record the message".into(),
-            ],
-            Style::default(),
-        ),
-    };
-
-    let text = Text::from(Line::from(msg)).patch_style(style);
-    let help_message = Paragraph::new(text);
-    f.render_widget(help_message, help_area);
-
-    let input = Paragraph::new(app.input.as_str())
-        .style(match app.input_mode {
-            InputMode::Normal => Style::default(),
-            InputMode::Editing => Style::default().fg(Color::Yellow),
-        })
-        .block(Block::bordered().title("Input"));
-    f.render_widget(input, input_area);
-
-    match app.input_mode {
-        InputMode::Normal => {}
-        InputMode::Editing => {
-            #[allow(clippy::cast_possible_truncation)]
-            f.set_cursor(
-                input_area.x + app.character_index as u16 + 1,
-                input_area.y + 1,
-            );
-        }
-    }
-
-    let list = List::new(app.files.clone())
-        .block(Block::bordered().title("files"))
-        .style(match app.input_mode {
-            InputMode::Normal => Style::default().fg(Color::Yellow),
-            InputMode::Editing => Style::default().fg(Color::White),
-        })
-        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-        .highlight_symbol(">>")
-        .repeat_highlight_symbol(true);
-
-    f.render_widget(list, message_area);
-} */
-use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
-use std::{fs, io};
-
-use ratatui::{prelude::*, widgets::StatefulWidget};
+use ratatui::prelude::*;
 
 use crossterm::{
     event::{self, Event, KeyCode, KeyEventKind},
@@ -281,15 +33,18 @@ struct App {
     input_mode: InputMode,
     message: Vec<String>,
     files: Vec<String>,
+    read_only_files: Vec<String>,
 }
 
 impl App {
-    const fn new(files: Vec<String>) -> Self {
+    fn new(files: Vec<String>) -> Self {
+        let files_clone = files.clone();
         Self {
             input: String::new(),
             input_mode: InputMode::Normal,
             message: Vec::new(),
             files,
+            read_only_files: files_clone,
             character_index: 0,
         }
     }
@@ -312,13 +67,14 @@ impl App {
     }
 
     fn filter_files(&mut self, input: String) {
-        let filtered_files: Vec<String> = self
-            .files
-            .iter()
-            .filter(|file| file.contains(&input))
-            .map(|file| file.clone())
-            .collect();
-        self.files = filtered_files;
+        let mut new_files: Vec<String> = Vec::new();
+
+        for file in self.read_only_files.iter() {
+            if file.contains(&input) {
+                new_files.push(file.clone());
+            }
+        }
+        self.files = new_files;
     }
 
     fn byte_index(&mut self) -> usize {
@@ -468,9 +224,25 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
     }
 }
 
+fn handle_file_selection(
+    file: &str,
+    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+) -> anyhow::Result<()> {
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+    Command::new("nvim")
+        .arg(file)
+        .status()
+        .expect("Failed to open file");
+    enable_raw_mode()?;
+    execute!(terminal.backend_mut(), EnterAlternateScreen)?;
+
+    println!("Selected file: {}", file);
+    Ok(())
+}
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Setup terminal
-    let entries = fs::read_dir("../../")?
+    let entries = fs::read_dir("./")?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
 
@@ -482,40 +254,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         file_strings.push(val.clone());
     }
 
-    /* enable_raw_mode()?;
-       let mut stdout = io::stdout();
-       execute!(stdout, EnterAlternateScreen)?;
-       let backend = CrosstermBackend::new(stdout);
-       let mut terminal = Terminal::new(backend)?;
-    */
     let mut app = App::new(file_strings.clone());
-    //let res = run_app(&mut terminal, app.clone());
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    // Create list items
-    /* let items = vec![
-        ListItem::new("Item 1"),
-        ListItem::new("Item 2"),
-        ListItem::new("Item 3"),
-    ]; */
-
     // Initial selected state
     let mut state = ListState::default();
     state.select(Some(0)); // Select the first item by default
-
-    //let mut input = String::new();
-
-    /* let items = vec![
-        "Item 1",
-        "Item 2",
-        "Item 3",
-        "Something else",
-        "Another item",
-    ]; */
 
     // Main loop
     loop {
@@ -525,9 +273,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .iter()
             .map(|file| ListItem::new(file.clone()))
             .collect();
-        //.filter(&item | item.to_lowercase().contains(app.input.to_lowercase()))
-        //.map(|&item| ListItem::new(item))
-        //.collect();
 
         // Draw UI
         terminal.draw(|f| {
@@ -629,10 +374,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         state.select(Some(i));
                     }
                     KeyCode::Enter => {
-                        //TODO: handle file selection
                         let selected = &app.files[state.selected().unwrap()];
 
                         app.input = selected.clone();
+
+                        let _ = handle_file_selection(selected, &mut terminal);
                     }
                     _ => {}
                 },
