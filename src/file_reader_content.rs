d308fd4ad76e33812599de6a5e55248b252b0877
Anthony Amaro <anthonyamaro5555@gmail.com>
2024-08-23 22:18:31 -0700

    feat: add capability to decompress zip files

--- src/file_reader_content.rs
+++ src/file_reader_content.rs
@@ -1,4 +1,4 @@
-use std::{fs, iter::zip, path::Path};
+use std::{fs, io, iter::zip, path::Path};
 
 #[derive(Debug, Clone)]
 pub enum FileType {
@@ -17,6 +17,8 @@ pub struct FileContent {
     pub error_message: String,
     pub curr_asset_path: String,
     pub curr_zip_content: Vec<String>,
+    pub curr_selected_path: String,
+    pub curr_csv_content: Vec<String>,
 }
 
 impl FileContent {
@@ -27,6 +29,8 @@ impl FileContent {
             error_message: String::from(""),
             curr_asset_path: String::from(""),
             curr_zip_content: Vec::new(),
+            curr_selected_path: String::from(""),
+            curr_csv_content: Vec::new(),
         }
     }
     pub fn is_curr_path_file(path: String) -> bool {
@@ -68,11 +72,32 @@ impl FileContent {
                     "js" | "ts" | "html" | "yml" | "json" | "css" => FileType::FILE,
                     "png" => FileType::IMG,
                     "zip" => FileType::ZIP,
+                    "csv" => FileType::CSV,
                     _ => FileType::NotAvailable,
                 }
             }
             None => FileType::NotAvailable,
         }
+    }
+
+    pub fn read_csv_content(&mut self) {
+        let file = fs::File::open(self.curr_selected_path.clone()).unwrap();
+
+        let mut rdr = csv::Reader::from_reader(file);
+
+        let mut file_content: Vec<String> = Vec::new();
+        for result in rdr.records() {
+            let record = result.unwrap();
+
+            for val in record.iter() {
+                file_content.push(val.to_string());
+                //println!("result: {:?}", val);
+            }
+        }
+
+        self.curr_csv_content = file_content;
+
+        //  for result in rdr.recors() {}
     }
 
     pub fn read_zip_content(&mut self, path: String) -> i32 {
@@ -98,5 +123,50 @@ impl FileContent {
 
         self.curr_zip_content = list;
         0
+    }
+
+    pub fn extract_zip_content(&mut self) -> String {
+        let fname = std::path::Path::new(&self.curr_selected_path);
+        let file = fs::File::open(fname).unwrap();
+
+        let mut archive = zip::ZipArchive::new(file).unwrap();
+
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i).unwrap();
+
+            let outpath = match file.enclosed_name() {
+                Some(f_path) => f_path,
+                None => continue,
+            };
+
+            {
+                let comment = file.comment();
+                if !comment.is_empty() {
+                    println!("File {i} comment: {comment}");
+                }
+            }
+
+            if file.is_dir() {
+                fs::create_dir_all(&outpath).unwrap();
+            } else {
+                if let Some(p) = outpath.parent() {
+                    if !p.exists() {
+                        fs::create_dir_all(p).unwrap();
+                    }
+                }
+
+                let mut outfile = fs::File::create(&outpath).unwrap();
+                io::copy(&mut file, &mut outfile).unwrap();
+            }
+
+            {
+                use std::os::unix::fs::PermissionsExt;
+
+                if let Some(mode) = file.unix_mode() {
+                    let _ = fs::set_permissions(&outpath, fs::Permissions::from_mode(mode));
+                }
+            }
+        }
+        self.curr_selected_path.clone()
     }
 }
