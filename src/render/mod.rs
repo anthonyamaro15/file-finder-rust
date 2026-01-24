92eb6c4532cf4bf58212480952cd9ccadbc9dbf8
Anthony <anthonyamaro5555@gmail.com>
2026-01-23 21:40:30 -0800

    feat: add more info for files to be deleted

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -6,7 +6,7 @@ pub mod size_bar;
 
 // Re-export items used by main.rs
 pub use popups::{
-    create_create_input_popup, create_delete_confirmation_block, create_keybindings_popup,
+    create_create_input_popup, create_delete_confirmation_popup, create_keybindings_popup,
     create_rename_input_popup, create_sort_options_popup, draw_popup, split_popup_area,
     split_popup_area_vertical,
 };
